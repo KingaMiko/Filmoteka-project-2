c622dcb802f9658ea9310fc4c1578269ae662fca
Kinga Mikołajczyk <kinga.mikola@gmail.com>
2023-06-11 21:40:18 +0200

    autocomplete: off, header input

--- src/searchbox.js
+++ src/searchbox.js
@@ -19,7 +19,7 @@ function findMovie(e) {
     headers: {
       accept: 'application/json',
       Authorization:
-        'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjOTc2OWMyZGE2MzlmZjllODU0YTM4N2VkMWI3Yjg5MSIsInN1YiI6IjY0ODJmNjFhZTM3NWMwMDBjNTI2Y2E2OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-e0pQI1sxEGJ4JEJ59Sreac2JKX6PA1NDAayyIXVyBg',
+        'Bearer Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjOTc2OWMyZGE2MzlmZjllODU0YTM4N2VkMWI3Yjg5MSIsInN1YiI6IjY0ODJmNjFhZTM3NWMwMDBjNTI2Y2E2OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-e0pQI1sxEGJ4JEJ59Sreac2JKX6PA1NDAayyIXVyBg',
     },
   };
 
