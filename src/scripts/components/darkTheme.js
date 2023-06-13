7bef5f172a9b6b6ae2e01eff0e8a26fd0121330f
Kinga Mikołajczyk <kinga.mikola@gmail.com>
2023-06-13 11:51:14 +0200

    localStorage for theme

--- src/scripts/components/darkTheme.js
+++ src/scripts/components/darkTheme.js
@@ -2,16 +2,33 @@ const bodyRef = document.querySelector('body');
 const toggleRef = document.querySelector('#theme-switch-toggle');
 const footerDarktheme = document.querySelector('.footer');
 
-toggleRef.addEventListener('change', () => {
-  if (bodyRef.classList.contains('dark-theme')) {
-    bodyRef.classList.remove('dark-theme');
-    bodyRef.classList.add('light-theme');
-    footerDarktheme.classList.remove('dark-theme');
-  } else {
-    bodyRef.classList.remove('light-theme');
+// Sprawdzenie, czy ustawienie motywu istnieje w localStorage
+const isDarkTheme = localStorage.getItem('darkTheme') === 'true';
+
+// Funkcja do ustawiania motywu
+const setTheme = darkTheme => {
+  if (darkTheme) {
     bodyRef.classList.add('dark-theme');
     footerDarktheme.classList.add('dark-theme');
+  } else {
+    bodyRef.classList.remove('dark-theme');
+    footerDarktheme.classList.remove('dark-theme');
   }
+};
+
+// Ustawienie motywu na podstawie wartości z localStorage lub domyślnej wartości
+setTheme(isDarkTheme);
+
+// Ustawienie stanu przełącznika
+toggleRef.checked = isDarkTheme;
+
+// Obsługa zmiany motywu
+toggleRef.addEventListener('change', () => {
+  const darkTheme = toggleRef.checked;
+  setTheme(darkTheme);
+
+  // Zapisanie ustawienia motywu w localStorage
+  localStorage.setItem('darkTheme', darkTheme);
 });
 
 export { bodyRef, toggleRef, footerDarktheme };
