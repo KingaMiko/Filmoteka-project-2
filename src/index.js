5023e684e972503c9210d20438fb8e23c3a3e45e
Kinga Mikołajczyk <115529387+KingaMiko@users.noreply.github.com>
2023-06-16 21:15:05 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import './sass/main.scss';
 import './scripts/api-service';
 import './scripts/fetch';
-// import './scripts/pagintaion';
 import './scripts/gallery';
-//import './scripts/open-close-modal';
 import './scripts/components/backToTop';
 import './scripts/searchbox';
 import './scripts/components/darkTheme';
 import './scripts/team';
 import './scripts/team-modal';
-//import './scripts/pagination';
-//import './scripts/loader';
-
-// import './scripts/trailer';
