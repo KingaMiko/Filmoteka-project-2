7a5c117f6c71dd8a4e64a425fbc3fa7aa9bc52da
YoananaoY <113176830+YoananaoY@users.noreply.github.com>
2023-06-21 12:48:24 +0200

    loaderANDfavicon

--- src/scripts/loader.js
+++ src/scripts/loader.js
@@ -1,11 +1,9 @@
 export function showLoader() {
   const loader = document.getElementById('loader');
-  loader.classList.add('loader--visible');
-  loader.classList.remove('loader--hidden');
+  loader.classList.add('loader');
 }
 
 export function hideLoader() {
   const loader = document.getElementById('loader');
-  loader.classList.add('loader--hidden');
-  loader.classList.remove('loader--visible');
+  loader.classList.remove('loader');
 }
